e37e4e58e7b969e0cca67445d318ec9474bef17a
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-02 06:47:19 -0700

    Create DB folder, Create all the project models empty files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import dbConnection from "./DB/dbConnection.js";
 const app = express();
 const port = 3000;
 
+dbConnection();
 app.listen(port, () => console.log(`running on port ${port}`));
