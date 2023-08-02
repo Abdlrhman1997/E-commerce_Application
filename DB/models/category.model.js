5c987c501176cea2b85c6b7bd98835161fb0e145
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-02 06:55:12 -0700

    Implement category model database

--- DB/models/category.model.js
+++ DB/models/category.model.js
@@ -1,2 +1,23 @@
+import { Schema, model } from "mongoose";
 
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    minlength: [1, "too short category name"],
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
 
+const categoryModel = model("category", categorySchema);
+
+export default categoryModel;
