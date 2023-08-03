74a73812340380d4d4c3f3e5e8a745555d371617
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-02 17:28:18 -0700

    Create modules, middleware, utils folders, Create bootstrap.js file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dbConnection from "./DB/dbConnection.js";
+import bootstrap from "./src/bootstrap.js";
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 dbConnection();
+bootstrap(app);
 app.listen(port, () => console.log(`running on port ${port}`));
