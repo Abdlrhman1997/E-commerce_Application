f1deef40a1f2a2ae7fb93ce1e5a027e7a2921320
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-05 16:17:59 -0700

    Create update categroy in category.controller, Setup it's routing in category.routes

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -6,3 +6,18 @@ export const addCategory = async (req, res, next) => {
   await category.save();
   res.status(201).json({ message: "success", category });
 };
+
+export const getAllCategories = async (req, res, next) => {
+  const categories = await categoryModel.find();
+  res.status(201).json({ message: "success", categories });
+};
+
+export const updateCategory = async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  req.body.slug = slugify(req.body.name);
+  const category = await categoryModel.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.status(201).json({ message: "success", category });
+};
--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -3,5 +3,7 @@ const router = Router();
 import * as categoryController from "./category.controller.js";
 
 router.post("/addCategory", categoryController.addCategory);
+router.get("/getAllCategories", categoryController.getAllCategories);
+router.put("/updateCategory/:id", categoryController.updateCategory);
 
 export default router;
