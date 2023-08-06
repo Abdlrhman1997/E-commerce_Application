56f1812818e34598a0fea5b59e21e0c3ad395e49
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-05 18:14:48 -0700

    Create function catchError in ErrorHandling.js to handle all the errors

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,10 @@
 import categoryRouter from "./modules/category/category.routes.js";
 const bootstrap = (app) => {
   app.use("/categories", categoryRouter);
+
+  app.use((err, req, res, next) => {
+    res.status(500).json(err);
+  });
 };
 
 export default bootstrap;
--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,31 +1,34 @@
 import categoryModel from "../../../DB/models/category.model.js";
 import slugify from "slugify";
-export const addCategory = async (req, res, next) => {
+import { catchError } from "../../utils/ErrorHandling.js";
+
+export const addCategory = catchError(async (req, res, next) => {
   req.body.slug = slugify(req.body.name);
   const category = new categoryModel(req.body);
   await category.save();
   res.status(201).json({ message: "success", category });
-};
+});
 
-export const getAllCategories = async (req, res, next) => {
+export const getAllCategories = catchError(async (req, res, next) => {
   const categories = await categoryModel.find();
   res.status(201).json({ message: "success", categories });
-};
+});
 
-export const updateCategory = async (req, res, next) => {
+export const updateCategory = catchError(async (req, res, next) => {
   const { id } = req.params;
   const { name } = req.body;
   req.body.slug = slugify(req.body.name);
   const category = await categoryModel.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  res.status(201).json({ message: "success", category });
-};
+  !category && res.status(404).json({ message: `category not found` });
+  category && res.status(200).json({ message: `success`, category });
+});
 
-export const deleteCategory = async (req, res, next) => {
+export const deleteCategory = catchError(async (req, res, next) => {
   const { id } = req.params;
   const category = await categoryModel.findByIdAndDelete(id);
 
   !category && res.status(404).json({ message: `category not found` });
   category && res.status(200).json({ message: `success`, category });
-};
+});
