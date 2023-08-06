444f8c7e7df5ccfdd3540e8294c739b8cbebc1e3
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-05 18:54:52 -0700

    Adjust error message in category.controller.js to be handeled by the new AppError class we created

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,6 +1,6 @@
 import categoryModel from "../../../DB/models/category.model.js";
 import slugify from "slugify";
-import { catchError } from "../../utils/ErrorHandling.js";
+import { AppError, catchError } from "../../utils/ErrorHandling.js";
 
 export const addCategory = catchError(async (req, res, next) => {
   req.body.slug = slugify(req.body.name);
@@ -21,7 +21,7 @@ export const updateCategory = catchError(async (req, res, next) => {
   const category = await categoryModel.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  !category && res.status(404).json({ message: `category not found` });
+  !category && next(new AppError("category doesn't exist", 404));
   category && res.status(200).json({ message: `success`, category });
 });
 
@@ -29,6 +29,6 @@ export const deleteCategory = catchError(async (req, res, next) => {
   const { id } = req.params;
   const category = await categoryModel.findByIdAndDelete(id);
 
-  !category && res.status(404).json({ message: `category not found` });
+  !category && next(new AppError("category doesn't exist", 404));
   category && res.status(200).json({ message: `success`, category });
 });
