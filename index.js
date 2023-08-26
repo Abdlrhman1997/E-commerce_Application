458d00f99e8f0da289422d74e0588b9e7cf367c5
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-25 17:22:56 -0700

    Add file upload code in index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bootstrap from "./src/bootstrap.js";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import { AppError } from "./src/middleware/ErrorHandling.js";
 dotenv.config();
 
 const app = express();
@@ -11,7 +13,24 @@ const port = 3000;
 app.use(express.json());
 app.use(morgan("dev"));
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + " - " + file.originalname);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("images only"), false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
 app.use(upload.single("img"));
 
 dbConnection();
