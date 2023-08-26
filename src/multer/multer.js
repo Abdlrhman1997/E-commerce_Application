4fe854291c7fa1e888f02537af89f7aa9c209e42
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-25 21:47:55 -0700

    Refactor multer code in multer.js

--- src/multer/multer.js
+++ src/multer/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import { AppError } from "../middleware/ErrorHandling.js";
 
-export const uploadSingleFile = (fileName, folderName) => {
+function myMulter(folderName) {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, `uploads/${folderName}`);
@@ -21,27 +21,11 @@ export const uploadSingleFile = (fileName, folderName) => {
   }
 
   const upload = multer({ storage, fileFilter });
-  return upload.single(fileName);
-};
+  return upload;
+}
 
-export const uploadMixedFiles = (arrayOfFields, folderName) => {
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, `uploads/${folderName}`);
-    },
-    filename: function (req, file, cb) {
-      cb(null, uuidv4() + "-" + file.originalname);
-    },
-  });
+export const uploadSingleFile = (fileName, folderName) =>
+  myMulter(folderName).single(fileName);
 
-  function fileFilter(req, file, cb) {
-    if (file.mimetype.startsWith("image")) {
-      cb(null, true);
-    } else {
-      cb(new AppError("images only"), false);
-    }
-  }
-
-  const upload = multer({ storage, fileFilter });
-  return upload.fields(arrayOfFields);
-};
+export const uploadMixedFiles = (arrayOfFields, folderName) =>
+  myMulter(folderName).fields(arrayOfFields);
