d53835d84150c0d087912ea7d23deadb9f1ef653
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-26 00:11:58 -0700

    Add token in signup

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -8,5 +8,9 @@ export const signUp = catchError(async (req, res, next) => {
   }
   const user = new userModel(req.body);
   await user.save();
-  res.status(201).json({ message: "success", user });
+  const token = jwt.sign(
+    { email: user.email, name: user.email, id: user._id, role: user.role },
+    "sasadanceonmsasa"
+  );
+  res.status(201).json({ message: "success", token });
 });
